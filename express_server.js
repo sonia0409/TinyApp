b400882f3f3b0346ae18de2ab7df31da040322b4
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-06 23:13:48 +0000

    render is added to the server and url_index.ejs file added

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,6 +20,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
+
+app.get("/urls",(req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("url_index",templateVars);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
