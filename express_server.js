b26c8878c28e98bdc400b3ed48ea33d58765f244
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-08 23:44:59 +0000

    registration functionality added, login ang lgout modified

--- express_server.js
+++ express_server.js
@@ -23,6 +23,38 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//users being stored
+const users = {
+  "id1": {
+    id: "id1",
+    email: "user1@example.com",
+    password: "123"
+  },
+  "id2": {
+    id: "id2",
+    email: "user2@example.com",
+    password: "abcd"
+  }
+};
+
+//generate id - random numbers;
+const generateRandomNumber = function() {
+const id = Math.floor((1 + Math.random()) * 3000);
+return id;
+};
+
+//find user by email
+const findUserByEmail = (email) => {
+  for(const userId in users) {
+    const user = users[userId];
+    if(user.email === email){
+      return user;
+    }
+  }
+  return null;
+}
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -36,24 +68,49 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const username = req.cookies.username;
-  const templateVars = { urls: urlDatabase, username};
+  const id = req.cookies.user_id;
+  const templateVars = { urls: urlDatabase, user: users[id]};
   res.render("urls_index", templateVars);
 });
 
 //route to direct to the registration form
 app.get("/register", (req, res) => {
-  res.render("register")
-   
+  const id = req.cookies.user_id;
+  const templateVars = { urls: urlDatabase, user: users[id]};
+  res.render("register", templateVars)
 });
 
+//route- handle the register post request
+app.post("/register", (req, res) => {
+  const id = generateRandomNumber();
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  //edge case- if email is empty - send 400 status code
+  if(!email || !password){
+    return res.status(400).send('email or password cannot be empty!!');
+  }
+  //edge case- if email already exists
+  const user = findUserByEmail(email);
+  
+  if(user){
+    return res.status(400).send('this email already exists!!')
+  }
 
+  users[id]= {
+    id: id,
+    email: email,
+    password: password
+  };
+  res.cookie("user_id", id)
+  res.redirect("/urls");
+});
 
 
 //this route will direct to the forms
 app.get("/urls/new", (req, res) => {
-  const username = req.cookies.username;
-  const templateVars = { urls: urlDatabase, username};
+  const id = req.cookies.user_id;
+  const templateVars = { urls: urlDatabase, user: users[id]};
   res.render("urls_new", templateVars);
 });
 
@@ -67,8 +124,8 @@ app.post("/urls", (req, res) => {
 
 //shows HTML file with the shortURL and longURL
 app.get("/urls/:shortURL", (req, res) => {
-  const username = req.cookies.username;
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username };
+  const id = req.cookies.user_id;
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[id]};
   res.render("urls_show", templateVars);
 });
 //redirecting the user to the lonURL website
@@ -91,20 +148,45 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+//login link renders new login page
+app.get("/login", (req, res) => {
+  const id = req.cookies.user_id;
+  const templateVars = { urls: urlDatabase, user: users[id]};
+  res.render("login", templateVars)
+});
 //route to connect with the login and collect the username cookie
 app.post("/login", (req, res) => {
- 
-  const username = req.body.username;
-  res.cookie('username', username )
-  res.redirect("/urls")
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if(!email || !password){
+    return res.status(400).send("email and password cannot be blank")
+  }
+  
+  const user = findUserByEmail(email)
+  
+  if(!user){
+    return res.status(403).send("email does not exist!");
+  }
+  
+  if(user.password !== password){
+    return res.status(403).send("password does not match!")
+  }
+
+
+  res.cookie("user_id", user.id)
+  res.redirect("/urls");
 
 });
 
 //route the loggout cookie to clear the cookie
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("user_id")
   res.redirect("/urls")
 });
+
+
 
 
 app.listen(PORT, () => {
