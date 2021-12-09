89e8f62a8e5fdd4f9a041dcd8f17b40d2e36c6f5
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-09 20:25:07 +0000

    encrypted password

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080;
 const bodyParser = require("body-parser");  // middleware added to accomodate POST request.
 const { urlencoded } = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true })); // directs the obtained LongURL to urls as a JSON(parsed).
@@ -13,9 +15,34 @@ app.set("view engine", "ejs");  //connect with the files in view folder with .ej
 
 
 
+//data
 
+//shortURL - longURL; key-value pair
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
+
+//users being stored
+const users = {
+  "id1": {
+    id: "id1",
+    email: "user1@example.com",
+    password: bcrypt.hashSync("123", 10)
+  },
+  "id2": {
+    id: "id2",
+    email: "user2@example.com",
+    password: bcrypt.hashSync("abcd", 10)
+  }
+};
 //functions
-
 //shortURL generated
 const generateRandomString = function() {
   // return a string of 6 random alphanumeric characters
@@ -47,7 +74,7 @@ const urlsForUser = (id) => {
       urlsWithSameUserID[user] = url;
     }
   }
-  return { error: "bad request", data: urlsWithSameUserID };
+  return {data: urlsWithSameUserID};
 };
 
 //check if id exists
@@ -60,41 +87,13 @@ const matchShortURL = (shortURL) => {
   return null;
 };
 
-//data
-
-//shortURL - longURL; key-value pair
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW"
-  }
-};
-
-//users being stored
-const users = {
-  "id1": {
-    id: "id1",
-    email: "user1@example.com",
-    password: "123"
-  },
-  "id2": {
-    id: "id2",
-    email: "user2@example.com",
-    password: "abcd"
-  }
-};
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json({ urlDatabase, users});
 });
 
 app.get("/hello", (req, res) => {
@@ -106,9 +105,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const id = req.cookies.user_id;
   const url = urlsForUser(id);
-
+  console.log("url",url);
   if (url.error) {
-    return res.status(400).redirect("/login");
+    url.error = "true";
+    return res.redirect("/login");
   }
   //if userID === user login id then display /urls page
   const templateVars = { urls: url.data, user: users[id] };
@@ -132,8 +132,12 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const hashedPassword = bcrypt.hashSync(password, 10)
+  
+  const passwordMatch = bcrypt.compareSync(password, hashedPassword) 
+
   //edge case- if email is empty - send 400 status code
-  if (!email || !password) {
+  if (!email || !passwordMatch) {
     return res.status(400).send('email or password cannot be empty!!');
   }
   //edge case- if email already exists
@@ -146,7 +150,7 @@ app.post("/register", (req, res) => {
   users[id] = {
     id: id,
     email: email,
-    password: password
+    password: hashedPassword
   };
   res.cookie("user_id", id);
   res.redirect("/urls");
@@ -237,8 +241,10 @@ app.post("/login", (req, res) => {
 
   const email = req.body.email;
   const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10)
+  const passwordMatch = bcrypt.compareSync(password, hashedPassword) 
 
-  if (!email || !password) {
+  if (!email || !passwordMatch) {
     return res.status(400).send("email and password cannot be blank");
   }
 
@@ -247,7 +253,7 @@ app.post("/login", (req, res) => {
     return res.status(403).send("email does not exist!");
   }
 
-  if (user.password !== password) {
+  if (!passwordMatch) {
     return res.status(403).send("password does not match!");
   }
 
