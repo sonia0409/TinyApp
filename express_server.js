14d12c1dff94365d9e530c165b40c788050ad67f
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-09 18:44:58 +0000

    ropermission features updated

--- express_server.js
+++ express_server.js
@@ -5,10 +5,16 @@ const bodyParser = require("body-parser");  // middleware added to accomodate PO
 const { urlencoded } = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true })); // directs the obtained LongURL to urls as a JSON(parsed).
 
 app.set("view engine", "ejs");  //connect with the files in view folder with .ejs
+
+
+
+
+
+//functions
 
 //shortURL generated
 const generateRandomString = function() {
@@ -16,11 +22,56 @@ const generateRandomString = function() {
   const shortURL = Math.floor((1 + Math.random()) * 0x100000).toString(16);
   return shortURL;
 };
+//generate id - random numbers;
+const generateRandomNumber = function() {
+  const id = Math.floor((1 + Math.random()) * 3000);
+  return id;
+};
+//find user by email
+const findUserByEmail = (email) => {
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+//find user by id; return -url
+const urlsForUser = (id) => {
+  const urlsWithSameUserID = {};
+  for (const user in urlDatabase) {
+    const url = urlDatabase[user];
+    if (id === url.userID) {
+      urlsWithSameUserID[user] = url;
+    }
+  }
+  return { error: "bad request", data: urlsWithSameUserID };
+};
+
+//check if id exists
+const matchShortURL = (shortURL) => {
+  for (const url in urlDatabase) {
+    if (shortURL === url) {
+      return shortURL;
+    }
+  }
+  return null;
+};
+
+//data
 
 //shortURL - longURL; key-value pair
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
 };
 
 //users being stored
@@ -37,23 +88,6 @@ const users = {
   }
 };
 
-//generate id - random numbers;
-const generateRandomNumber = function() {
-const id = Math.floor((1 + Math.random()) * 3000);
-return id;
-};
-
-//find user by email
-const findUserByEmail = (email) => {
-  for(const userId in users) {
-    const user = users[userId];
-    if(user.email === email){
-      return user;
-    }
-  }
-  return null;
-}
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -67,17 +101,29 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
 app.get("/urls", (req, res) => {
   const id = req.cookies.user_id;
-  const templateVars = { urls: urlDatabase, user: users[id]};
+  const url = urlsForUser(id);
+
+  if (url.error) {
+    return res.status(400).redirect("/login");
+  }
+  //if userID === user login id then display /urls page
+  const templateVars = { urls: url.data, user: users[id] };
   res.render("urls_index", templateVars);
+
 });
+
+
+
 
 //route to direct to the registration form
 app.get("/register", (req, res) => {
   const id = req.cookies.user_id;
-  const templateVars = { urls: urlDatabase, user: users[id]};
-  res.render("register", templateVars)
+  const templateVars = { urls: urlDatabase, user: users[id] };
+  res.render("register", templateVars);
 });
 
 //route- handle the register post request
@@ -85,24 +131,24 @@ app.post("/register", (req, res) => {
   const id = generateRandomNumber();
   const email = req.body.email;
   const password = req.body.password;
-  
+
   //edge case- if email is empty - send 400 status code
-  if(!email || !password){
+  if (!email || !password) {
     return res.status(400).send('email or password cannot be empty!!');
   }
   //edge case- if email already exists
   const user = findUserByEmail(email);
-  
-  if(user){
-    return res.status(400).send('this email already exists!!')
+
+  if (user) {
+    return res.status(400).send('this email already exists!!');
   }
 
-  users[id]= {
+  users[id] = {
     id: id,
     email: email,
     password: password
   };
-  res.cookie("user_id", id)
+  res.cookie("user_id", id);
   res.redirect("/urls");
 });
 
@@ -110,14 +156,24 @@ app.post("/register", (req, res) => {
 //this route will direct to the forms
 app.get("/urls/new", (req, res) => {
   const id = req.cookies.user_id;
-  const templateVars = { urls: urlDatabase, user: users[id]};
-  res.render("urls_new", templateVars);
+  const templateVars = { urls: urlDatabase, user: users[id] };
+
+  const user = users[id];
+
+  if (user) {
+    res.render("urls_new", templateVars);
+  }
+  if (!user) {
+    return res.redirect("/login");
+  }
+
 });
 
 //POST redirects to the HTML page.
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  const id = req.cookies.user_id;
+  urlDatabase[shortURL] = { longURL: req.body.longURL, userID: id };
   res.redirect(`/urls/${shortURL}`);
 
 });
@@ -125,13 +181,33 @@ app.post("/urls", (req, res) => {
 //shows HTML file with the shortURL and longURL
 app.get("/urls/:shortURL", (req, res) => {
   const id = req.cookies.user_id;
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[id]};
-  res.render("urls_show", templateVars);
+  const shortURL = req.params.shortURL;
+
+  const url = matchShortURL(shortURL);
+  if (url) {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    const templateVars = { shortURL: shortURL, longURL: longURL, user: users[id] };
+    res.render("urls_show", templateVars);
+  }
+  if (!url) {
+    res.status(400).send("this url doesnot exist");
+  }
+
 });
-//redirecting the user to the lonURL website
+
+//redirecting the user to the longURL website
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+
+  const shortURL = req.params.shortURL;
+  const url = matchShortURL(shortURL);
+  if (url) {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(longURL);
+  }
+  if (!url) {
+    res.status(400).send("this url doesnot exist");
+  }
+
 });
 
 //route to delete the urls
@@ -140,19 +216,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-//route to update the longURL
+//route to edit the longURL
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
+  const id = req.cookies.user_id;
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
+
+  urlDatabase[shortURL] = { longURL: longURL, userID: id };
   res.redirect("/urls");
 });
 
 //login link renders new login page
 app.get("/login", (req, res) => {
   const id = req.cookies.user_id;
-  const templateVars = { urls: urlDatabase, user: users[id]};
-  res.render("login", templateVars)
+  const templateVars = { urls: urlDatabase, user: users[id] };
+  res.render("login", templateVars);
 });
 //route to connect with the login and collect the username cookie
 app.post("/login", (req, res) => {
@@ -160,30 +238,28 @@ app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if(!email || !password){
-    return res.status(400).send("email and password cannot be blank")
+  if (!email || !password) {
+    return res.status(400).send("email and password cannot be blank");
   }
-  
-  const user = findUserByEmail(email)
-  
-  if(!user){
+
+  const user = findUserByEmail(email);
+  if (!user) {
     return res.status(403).send("email does not exist!");
   }
-  
-  if(user.password !== password){
-    return res.status(403).send("password does not match!")
+
+  if (user.password !== password) {
+    return res.status(403).send("password does not match!");
   }
 
-
-  res.cookie("user_id", user.id)
+  res.cookie("user_id", user.id);
   res.redirect("/urls");
 
 });
 
 //route the loggout cookie to clear the cookie
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id")
-  res.redirect("/urls")
+  res.clearCookie("user_id");
+  res.redirect("/urls");
 });
 
 
